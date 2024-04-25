70f5b4f0236475bc4522aba9928a61de4aab1108
Marcell Varga <themarcellvarga@gmail.com>
2024-04-26 00:29:52 +0200

    Index page content added

---
+++ .history/app/page_20240423012008.tsx
@@ -0,0 +1,166 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import IndexSigAnimated from "../public/icons/indexSigAnimated";
+
+// Header component
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header className="flex justify-between items-center w-full max-w-5xl">
+      <div className="flex items-center">
+        {/* Icon */}
+        <Link href="/">
+          <IndexSigAnimated />
+          {/* <Image
+            src="/icons/indexSig.svg"
+            alt="Marcell's signature as Index button"
+            width={85}
+            height={37}
+          /> */}
+        </Link>
+      </div>
+      <nav className="flex items-center">
+        {/* Hamburger Menu */}
+        <div className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
+          <div className="w-6 h-0.5 bg-gray-800 mb-1"></div>
+          <div className="w-6 h-0.5 bg-gray-800 mb-1"></div>
+          <div className="w-6 h-0.5 bg-gray-800"></div>
+        </div>
+        {/* Navigation Links */}
+        <div className={`${isOpen ? "block" : "hidden"} md:flex space-x-4`}>
+          <Link href="/about">
+            <div className="text-sm font-medium text-custom-blue">About</div>
+          </Link>
+          <Link href="/work">
+            <div className="text-sm font-medium text-custom-blue">Work</div>
+          </Link>
+          <Link href="/contact">
+            <div className="text-sm font-medium text-custom-blue">Contact</div>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+// Footer component
+const Footer = () => {
+  return (
+    <div className="flex w-full flex-row justify-between items-center">
+      <div className="flex w-full">© 2024 Marcell Varga</div>
+      <div className="flex space-x-4">
+        <Image
+          src="/icons/linkedin.svg"
+          alt="Linkedin Icon"
+          width={24}
+          height={24}
+        />
+        <Image
+          src="/icons/github.svg"
+          alt="GitHub Icon"
+          width={24}
+          height={24}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default function Page() {
+  return (
+    <div className="m-4">
+      <Header />
+      <main className="flex flex-col items-center justify-between h-screen m-4 gap-1 w-full">
+        <div className="flex-grow py-12 w-full flex flex-col items-center justify-center gap-">
+          <div className="flex w-full text-5xl font-medium items-center justify-start">
+            Hey, I'm&nbsp;
+            <span className="text-custom-blue font-bold">Marcell Varga</span>
+          </div>
+          <div className="flex w-full text-3xl font-light items-center justify-start">
+            UX & Frontend engineer
+          </div>
+        </div>
+        <div className="flex-grow py-12 flex flex-col items-center justify-center">
+          <Image
+            className="animate-moveDown"
+            src="/icons/mouseScroll.svg"
+            alt="Scroll your mouse down Icon"
+            width={24}
+            height={24}
+          />
+        </div>
+      </main>
+      {/* Drive Section */}
+      <section className="mt-8">
+        <h2 className="text-lg font-light mb-4">
+          An adventurous UX & Frontend engineer with an enthusiasm for crafting
+          delightful digital experiences. I thrive on solving complex problems
+          through efficient design and love turning challenges into
+          opportunities. Overcoming challenges through efficient design is what
+          fuelling my everyday drive.
+        </h2>
+      </section>
+      {/* New Experience Section */}
+      <section className="mt-8">
+        <h2 className="text-custom-blue text-sm font-bold mb-4 tracking-wider uppercase">
+          Experience
+        </h2>
+        <div className="flex flex-wrap gap-2 ">
+          {[
+            "React",
+            "Figma",
+            "Sketch",
+            "JavaScript",
+            "TypeScript",
+            "Next.js",
+            "Node.js",
+            "Express.js",
+            "HTML",
+            "CSS",
+            "UX Design",
+            "UX Research",
+            "RESTful APIs",
+            "Git (Gitlab & Github)",
+            "Svelte & SvelteKit",
+            "Tailwind CSS",
+            "Adobe CC Suite",
+          ].map((skill) => (
+            <div
+              key={skill}
+              className="bg-custom-blue text-custom-teal px-2.5 py-1 rounded-lg"
+            >
+              {skill}
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="mt-8">
+        <h2 className="text-custom-blue text-sm font-bold mb-4 tracking-wider uppercase">
+          Projects
+        </h2>
+        <div className="flex flex-wrap gap-2">
+          {[
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "TypeScript",
+            "React-Bootstrap",
+            "Fluent UI",
+          ].map((skill) => (
+            <div
+              key={skill}
+              className="bg-custom-blue text-custom-teal px-2.5 py-1 rounded-lg"
+            >
+              {skill}
+            </div>
+          ))}
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
+}
---
+++ app/(work)/askcody/page.tsx
@@ -0,0 +1,13 @@
+// work.tsx
+import React from "react";
+import Link from "next/link";
+
+const AskCody = () => {
+  return (
+    <>
+      <div>AskCody</div>
+    </>
+  );
+};
+
+export default AskCody;
