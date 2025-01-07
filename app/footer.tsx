386711c7a6a052e0ccd9c8a3d7c2cf6bc70f4ab2
Marcell Varga <themarcellvarga@gmail.com>
2025-01-07 23:59:12 +0100

    Update footer to display the current year dynamically

--- app/footer.tsx
+++ app/footer.tsx
@@ -17,7 +17,7 @@ const Footer: React.FC<FooterProps> = ({ isHover }) => {
       <div className="flex w-full flex-col">
         <div className="w-full border-t border-gray-300 opacity-60"></div>
         <div className="flex w-full flex-row justify-between items-center px-4 pt-4">
-          <div className="flex text-gray-400 w-full text-sm">© 2025 Marcell Varga</div>
+          <div className="flex text-gray-400 w-full text-sm">© {new Date().getFullYear()} Marcell Varga</div>
           <div className="flex space-x-4">
             <Link href="https://www.linkedin.com/in/marcellvarga/" target="_blank" rel="noopener noreferrer">
               <LinkedInIcon className={`transition-colors duration-300 ${isHover ? "text-blue-600" : "text-gray-500"} hover:text-blue-600`} />
