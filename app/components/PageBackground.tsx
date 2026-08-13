b5a4538edd294c65ea1b646c5641962f82ecb415
Marcell Varga <themarcellvarga@gmail.com>
2026-08-13 12:00:33 +0800

    Revert "polish: remove generic portfolio UI patterns"
    This reverts commit 0e0488adf030d9d9b95c0990377c5a4db9422f17.

--- app/components/PageBackground.tsx
+++ app/components/PageBackground.tsx
@@ -1,5 +1,8 @@
 export function PageBackground() {
   return (
-    <div className="pointer-events-none fixed inset-0 -z-20 bg-[linear-gradient(145deg,_#f8f2ea_0%,_#f3eee7_48%,_#faf6f0_100%)]" />
+    <>
+      <div className="pointer-events-none fixed inset-0 -z-20 bg-[radial-gradient(circle_at_top_left,_rgba(76,207,255,0.16),_transparent_28%),radial-gradient(circle_at_20%_70%,_rgba(255,153,102,0.16),_transparent_34%),radial-gradient(circle_at_85%_18%,_rgba(17,27,40,0.12),_transparent_24%),linear-gradient(180deg,_#f8f1e8_0%,_#f6efe5_46%,_#fff8f1_100%)]" />
+      <div className="pointer-events-none fixed inset-0 -z-5 bg-[radial-gradient(circle_at_top_left,_rgba(76,207,255,0.10),_transparent_28%),radial-gradient(circle_at_20%_70%,_rgba(255,153,102,0.08),_transparent_34%)]" />
+    </>
   );
 }
